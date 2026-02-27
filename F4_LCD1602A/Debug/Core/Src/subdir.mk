abe7d80e67bd7b75c8516afd9fbb562ef58e1de9
Troitskaya Tamara <troitskayatamara@mail.ru>
2026-02-27 08:50:22 +0300

    LCD 1602A + I2C (PCF8574T), CMSIS. Working code. Need to understand what's changed. And rewrite it again from the scratch.

---
+++ F4_LCD1602A/Debug/Core/Src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/main.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c 
+
+OBJS += \
+./Core/Src/main.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o 
+
+C_DEPS += \
+./Core/Src/main.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F446xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su
+
+.PHONY: clean-Core-2f-Src
+
